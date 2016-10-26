536b66a8c4b7875294e9de02752d672d89d0f66f
HelloWoreld <Zezhong1234@163.com>
2016-10-26 12:32:42 +0800

    SendMessageTest

---
+++ SendMessage/SendMessage/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace SendMessage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Send = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SendAdress = new System.Windows.Forms.TextBox();
+            this.SendContent = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SendPerson = new System.Windows.Forms.TextBox();
+            this.SendTiltle = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Send
+            // 
+            this.Send.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Send.Location = new System.Drawing.Point(177, 316);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(85, 33);
+            this.Send.TabIndex = 0;
+            this.Send.Text = "发 送 邮 件";
+            this.Send.UseVisualStyleBackColor = true;
+            this.Send.Click += new System.EventHandler(this.Send_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(138, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "邮件伪造发信测试工具";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(34, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 14);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "收件人（邮箱地址）：";
+            // 
+            // SendAdress
+            // 
+            this.SendAdress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SendAdress.Location = new System.Drawing.Point(177, 56);
+            this.SendAdress.Name = "SendAdress";
+            this.SendAdress.Size = new System.Drawing.Size(254, 21);
+            this.SendAdress.TabIndex = 3;
+            // 
+            // SendContent
+            // 
+            this.SendContent.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SendContent.Location = new System.Drawing.Point(37, 166);
+            this.SendContent.Multiline = true;
+            this.SendContent.Name = "SendContent";
+            this.SendContent.Size = new System.Drawing.Size(394, 135);
+            this.SendContent.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(34, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 14);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "发信内容：";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(34, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 14);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "想要伪造的发件人：";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(34, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 14);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "伪造的邮件的主题：";
+            // 
+            // SendPerson
+            // 
+            this.SendPerson.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SendPerson.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.SendPerson.Location = new System.Drawing.Point(177, 87);
+            this.SendPerson.Name = "SendPerson";
+            this.SendPerson.Size = new System.Drawing.Size(254, 21);
+            this.SendPerson.TabIndex = 8;
+            // 
+            // SendTiltle
+            // 
+            this.SendTiltle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SendTiltle.Location = new System.Drawing.Point(177, 114);
+            this.SendTiltle.Name = "SendTiltle";
+            this.SendTiltle.Size = new System.Drawing.Size(254, 21);
+            this.SendTiltle.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 361);
+            this.Controls.Add(this.SendTiltle);
+            this.Controls.Add(this.SendPerson);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SendContent);
+            this.Controls.Add(this.SendAdress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Send);
+            this.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(486, 399);
+            this.Name = "Form1";
+            this.Text = "欢 迎 使 用 ，请 勿 用 于 非 法 行 为 !";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox SendAdress;
+        private System.Windows.Forms.TextBox SendContent;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox SendPerson;
+        private System.Windows.Forms.TextBox SendTiltle;
+    }
+}
+
---
+++ SendMessage/SendMessage/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SendMessage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Send_Click(object sender, EventArgs e)
+        {
+            string SendAdr = this.SendAdress.Text;
+            string SendPer = this.SendPerson.Text;
+            string SendCon = this.SendContent.Text;
+            string SendTil = this.SendTiltle.Text;
+            Boolean flag = true;
+            if (string.IsNullOrEmpty(SendAdr.Trim()) || string.IsNullOrEmpty(SendPer.Trim()) || string.IsNullOrEmpty(SendCon.Trim()) || string.IsNullOrEmpty(SendTil.Trim()))
+            {
+                MessageBox.Show("请完善所有信息！");
+            }
+            else
+            {
+                Regex regex = new Regex(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*");
+                if (!regex.IsMatch(SendPer.Trim()) || !regex.IsMatch(SendAdr.Trim()))
+                {
+                    MessageBox.Show("Email不合法,请重新输入!");
+                }
+                else
+                {
+                    System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient();
+                    client.Host = "";//使用163的SMTP服务器发送邮件
+                    client.UseDefaultCredentials = false;
+                    client.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;//通过网络发送到SMTP服务器
+                    client.Credentials = new System.Net.NetworkCredential("", "");//163的SMTP服务器需要用163邮箱的用户名和密码作认证，如果没有需要去163申请个,                                                                         
+                    //这里假定你已经拥有了一个163邮箱的账户，用户名为abc，密码为******* 
+                    System.Net.Mail.MailMessage Message = new System.Net.Mail.MailMessage();
+                    Message.From = new System.Net.Mail.MailAddress(SendPer);//这里需要注意，163似乎有规定发信人的邮箱地址必须是163的，而且发信人的邮箱用户名必须和上面SMTP服务器认证时的用户名相同                                                               
+                    //因为上面用的用户名abc作SMTP服务器认证，所以这里发信人的邮箱地址也应该写为abc@163.com
+                    Message.To.Add(SendAdr);//将邮件发送给Gmail
+                    Message.Subject = SendTil;//邮件主题
+                    Message.Body = SendCon;//邮件内容
+                    Message.SubjectEncoding = System.Text.Encoding.UTF8;//编码
+                    Message.BodyEncoding = System.Text.Encoding.UTF8;
+                    Message.Priority = System.Net.Mail.MailPriority.High;//设置邮件优先级
+                    Message.IsBodyHtml = true;//设置为HTML格式
+                    try
+                    {
+                        client.Send(Message);
+
+                    }
+                    catch
+                    {
+                        flag = false;
+
+                    }
+                    if (flag)
+                    {
+                        MessageBox.Show("发送成功O(∩_∩)O~");
+                    }
+                    else
+                    {
+                        MessageBox.Show("发送失败/(ㄒoㄒ)/~~");
+                    }
+                }
+            }
+        }
+    }
+}
